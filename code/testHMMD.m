246e72bc27c19c0d8880e1753db6e05be3e97ee8
Antoine Honore <antoinehonor@gmail.com>
2015-09-25 12:56:44 +0200

    testHMMD

--- code/testHMMD.m
+++ code/testHMMD.m
@@ -3,24 +3,31 @@
 clear all
 %source model 1
 mc=MarkovChain([1 0],[0.9 0.1 0;0 0.9 0.1]);
-pD(1)=DiscreteD([0.1 0.2 0.6]);%state 1
-pD(2)=DiscreteD([0.6 0.3 0.1]);%state 2
+pD(1)=DiscreteD([0.5 0.45 0.05]);%state 1
+pD(2)=DiscreteD([0.6 0.35 0.05]);%state 2
 %h(1)=HMM('MarkovChain',mc,'OutputDistr',pD);
 h(1)=HMM(mc,pD);
 
 %source model 2
 mc=MarkovChain([1 0],[0.95 0.05 0;0 0.8 0.2]);
-%pD(1)=DiscreteD([0.2 0.6 0.2]);%state 1
-pD2(1)=GaussD;
-pD2(2)=GaussD;
-%pD(2)=DiscreteD([0.4 0.2 0.4]);%state 2
+pD(1)=DiscreteD([0.2 0.6 0.2]);%state 1
+pD(2)=DiscreteD([0.4 0.2 0.4]);%state 2
+
+%pD2(1)=GaussD;
+%pD2(2)=GaussD;
+
 %h(2)=HMM('MarkovChain',mc,'OutputDistr',pD);
-h(2)=HMM(mc,pD2);
+h(2)=HMM(mc,pD);
 
 %test models:
+nSamples = 10000;
 for r=1:5
-    [x1,zTest1]=rand(h(1),1000);
-    [x2,zTest2]=rand(h(2),1000);
+    [x1,zTest1]=rand(h(1),nSamples);
+    [x2,zTest2]=rand(h(2),nSamples);
     %test1=logprob(h,zTest1)
     %test2=logprob(h,zTest2)
 end;
+mx1 = mean(x1,2);
+mx2 = mean(x2,2);
+t1 = size(zTest1,2); t2 = size(zTest2,2);
+figure(1), plot(1:t1, mx1);
