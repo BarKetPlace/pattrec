ac6b2fc7e356e8d9cbd097c7d0bee060de269630
Antoine Honore <antoinehonor@gmail.com>
2015-10-02 22:43:22 +0200

    improve

--- code/main.m
+++ code/main.m
@@ -16,13 +16,6 @@ nbSamples = sum(size(S))-1;
 %%%%%%%%%%%%%%%%
 % Plot & Play the sound
 %%%%%%%%%%%%%%%%
-
-%display_(S, Fs, 1, mute);
-
-%%%%%%%%%%%%%%%%
-% fft
-%%%%%%%%%%%%%%%%
-
 figure(1),
 Y = fft(S);
 plot([0:1/nbSamples:0.5-1/nbSamples],Y(1:nbSamples/2)); xlim([0 0.3]);
@@ -33,3 +26,18 @@ figure(2); subplot(2,1,1)
 spectrogram(S, 200, 50, 200, Fs, 'yaxis'); colorbar('off');
 subplot(2,1,2);
 display_(S, Fs, scaling_f, 0);
+
+%% 
+%%%%%%%%%%%%%%%%
+% features extraction
+%%%%%%%%%%%%%%%%
+
+
+wind_size_sample = 20000;
+frIsequence = GetMusicFeatures(S, Fs, wind_size_sample/Fs);
+frIsequence
+
+figure,
+scaling_f = 1;
+display_(S, Fs, scaling_f, 1); hold on;
+stem(wind_size_sample*[0:floor(nbSamples/wind_size_sample)], ones(floor(nbSamples/wind_size_sample)+1), 'filled');
