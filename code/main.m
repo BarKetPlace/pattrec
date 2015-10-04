e06766007f8efa488f8e5cfd97fdb93e6b99d154
Antoine Honore <antoinehonor@gmail.com>
2015-10-04 17:41:03 +0200

    modifications & new song

--- code/main.m
+++ code/main.m
@@ -7,33 +7,49 @@
 clear all
 clc
 close all
-
+% Recording/reading folder
 path = '../songs/';
-filename = 'melody_3.wav';
-[S Fs] = audioread(strcat(path,filename));
+
+fromfile = 0;
+if fromfile
+    filename = 'marseillaise1.wav';
+    [S Fs] = audioread(strcat(path,filename));
+else
+    % Record your voice for 5 seconds.
+    recObj = audiorecorder;
+    fprintf('record in :: ')
+    fprintf('3'); pause(1); fprintf('\b2'); pause(1); fprintf('\b1'); pause(1);
+    fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\bStart speaking!')
+    recordblocking(recObj, 5);
+    disp('End of Recording.');
+    S = getaudiodata(recObj)
+    Fs = recObj.SampleRate;
+    audiowrite(strcat(path,'highwaytohell.wav'), S, Fs);
+end
 nbSamples = sum(size(S))-1;
 
 % Fourier Transform
 Y = fft(S);
+scaling_f = 1;
+plot(scaling_f*[0:1/nbSamples:0.5-1/nbSamples],Y(1:nbSamples/2));% xlim([0 0.3]);
+xlabel('Normalized frequencies'); ylabel('|FT(signal)|^2'); title('Fourier transform');
 
 %%%%%%%%%%%%%%%%
 % Plot & Play the sound
 %%%%%%%%%%%%%%%%
 figure(1),
-plot(Fs*[0:1/nbSamples:0.5-1/nbSamples],Y(1:nbSamples/2));% xlim([0 0.3]);
-xlabel('Normalized frequencies'); ylabel('|FT(signal)|^2'); title('Fourier transform');
-
 scaling_f = Fs;
-mute =1;
+mute = 0;
 
-figure(2); subplot(3,1,1)
+figure(2); subplot(2,1,1)
 spectrogram(S, 200, 50, 200, Fs, 'yaxis'); colorbar('off');
-subplot(3,1,2);
+title('Frequency vs time');
+subplot(2,1,2);
 display_(S, Fs, scaling_f, mute);
 
 %% 
 %%%%%%%%%%%%%%%%
-% features extraction
+% Features extraction
 %%%%%%%%%%%%%%%%
 
 wind_size_sample = 10000;
@@ -52,13 +68,12 @@ for i=1:nbFrames
 end
 m_tmp = zeros(1,nbFrames);
 m_ = zeros(1,nbFrames);
-figure(3), plot(x);
+figure(3), plot(x); title('Pitch with a threshold on intensity');
 %% 
 j=1; flag=0; i=1;
-figure(4), clf
 
 while i<=nbFrames
-    while (x(1,i)~=0)
+    while (i<=nbFrames && x(1,i)~=0)
         if (~flag)  start = i; end
         flag = 1;
         i=i+1;
@@ -66,7 +81,7 @@ while i<=nbFrames
     if flag
         stop = i-1;
         mean(x(1,start:stop));
-        m_(1,start:stop) = mean(x(1,start:stop));
+        m_(1,start:stop) = median(x(1,start:stop));
         flag = 0;
         j=j+1;
     end
@@ -74,7 +89,7 @@ while i<=nbFrames
 i=i+1;
 end
 
-figure, plot(m_);
+figure, plot(m_); title('Meaned pitches');
 
 % %% record
 % % Record your voice for 5 seconds.
