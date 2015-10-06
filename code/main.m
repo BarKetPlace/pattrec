af4d7c33fa42195221f56bd57b500903ecfd40b9
Antoine Honore <antoinehonor@gmail.com>
2015-10-06 14:36:12 +0200

    distribution evaluation (ksdensity function in matlab)

--- code/main.m
+++ code/main.m
@@ -11,10 +11,11 @@ close all
 path = '../songs/';
 
 % Parameters
-fromfile = 1; % Read a prerecorded file (1) or record one (0)
-    filename = 'melody_3.wav'; % Which one
+fromfile = 1; % Read a pre-recorded file (1) or record one (0)
+    filename = 'melody_3.wav';
     mute = 0; % Listen to the file
-    
+
+Fs=0; %We do not know the value so far
 scaling_f = Fs; % Put Fs and the plot will be in seconds, put 1 to let the number of samples
 
 
@@ -54,28 +55,33 @@ display_(S, Fs, scaling_f, mute);
 %%%%%%%%%%%%%%%%
 % Features extraction
 %%%%%%%%%%%%%%%%
-frIsequence = GetMusicFeatures(S, Fs, 0.03);
+
+frIsequence = GetMusicFeatures(S, Fs, 0.02);
 nbFrames=size(frIsequence,2);
 max_intensity = max(frIsequence(3,:));
+
+
+%Removing the silence
+threshold = 0.2;%We assume that sounds with intensity >= threshold*max_intensity contains information
 % Perform a threshold on the intensity, the pitch of the frame with an ...
 %   intensity >= alpha*max_intensity are copied, the other pitch are
 %   discarded(put to 0)
 x=zeros(1,nbFrames); % Will receive the new pitches
 
 for i=1:nbFrames
-    if frIsequence(3,i)>0.2*max_intensity
-        x(1,i) = frIsequence(1,i);
+    if frIsequence(3,i)>=threshold*max_intensity
+        x(1,i) = frIsequence(1,i); %Save em !
     else
-        x(1,i)=0;
+        x(1,i)=0; %Discard them !
     end   
 end
 figure(3), plot(x); title('Pitch with a threshold on intensity');
-%% This part compute the median of the pitch when the pitch is ~=0
-m_tmp = zeros(1,nbFrames);
+%% This part compute the median of the pitch when the pitch is ~= 0
 m_ = zeros(1,nbFrames); %Will receive the result
 flag=0; %The flag = 1 when the x(1,i) ~=0 (the intensity was reasonable, see previous section)
 i=1;
-
+j=1; %count the number of pitches
+figure, title('Plot all the distribution of the pitch gathered in the same state');
 while i<=nbFrames
     while (i<=nbFrames && x(1,i)~=0)
         if (~flag)  start = i; end
@@ -86,9 +92,20 @@ while i<=nbFrames
         stop = i-1;
         mean(x(1,start:stop));
         m_(1,start:stop) = median(x(1,start:stop));
+        pitch(j) = m_(1,start);%save the value of the pitch
+        [b(j,:) xb(j,:)] = ksdensity(x(1,start:stop)); %b(i,:) contains the probability for the xb(i,:) values
+        plot(xb(j,:), b(j,:)); hold on;
+        j=j+1;
         flag = 0;
     end
 i=i+1;
 end
-
 figure, plot(m_); title('Medianed pitches');
+% plot_nb = 6;
+% figure, plot(xb(plot_nb,:), b(plot_nb,:));
+%%
+lowest = 20;
+q=2;
+octaves = log(lowest*q.^[0:9])
+pitch_log=log(pitch);
+Rounded_pitches = interp1(octaves,octaves,pitch_log,'nearest')
