236e01281db6209202c7db573c8653511631f1df
Antoine Honore <antoinehonor@gmail.com>
2015-10-13 14:29:23 +0200

    new files & start of assignnment 3

---
+++ code/main_back.m
@@ -0,0 +1,25 @@
+%----------------------------------------------------
+%Code Authors:
+% Antoine Honoré
+% Audrey Brouard
+%----------------------------------------------------
+
+
+clear all
+close all
+% Definition of the HMM
+q = [1;0];
+A = [.9, .1, 0; 0, .9, .1];
+
+mc = MarkovChain(q,A);
+mu_1 = 0; sig_1 = 1;
+mu_2 = 3; sig_2 = 2;
+pDgen(1)=GaussD('Mean',[mu_1],'StDev',[sig_1]);
+pDgen(2)=GaussD('Mean',[mu_2],'StDev',[sig_2]);
+h = HMM(mc, pDgen);
+% Observation
+X = [-.2, 2.6, 1.3];
+% Scale factor
+c = [1, .1625, .8266, .0581];
+[s, tmp] = pDgen.prob(X(3))
+% betaHat=backward(mc, pX, c);
