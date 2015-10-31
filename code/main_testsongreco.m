713448d86a304c7abc41e8ada5e13c3331cba442
Antoine Honore <antoinehonor@gmail.com>
2015-10-31 13:43:20 +0100

    nv main & files

--- code/main_testsongreco.m
+++ code/main_testsongreco.m
@@ -8,33 +8,35 @@ clear all
 close all
 clc
 
-
-files_loc = '../songs/';
-
-files = dir(files_loc);
-% Get a logical vector that tells which one is a directory.
-dirFlags = [files.isdir];
-% Extract only those that are directories.
-subFolders = files(dirFlags);
-file_name = 'concerninghobbits_00.wav';
+test_path = '../songs/tests/'
+file_name = 'marseillaise_04.wav';
 user_mode = 'load';
-good=0;
-
-% load the different hmm from the available data base
+nfiles = length(dir(fullfile([test_path '*_*.wav'])));
+list_file = dir(fullfile(test_path));
+list_file = {list_file.name};
+%% load the different hmm from the available data base
 fprintf('Loading hmms ...\n');
 files_loc = '../songs/';
 [ hmms ] = load_hmms( files_loc );
 fprintf('\b\b\b\bcompleted (%d hmms are loaded).\n', length(hmms));
-% launch recognition system
-[pathRes, probRes] = songreco(hmms, user_mode, file_name);
-fprintf('Result :: %s\n',pathRes);
-
-% is it the right result
-if strcmp(user_mode,'load')
-    res = findstr(file_name(1:end-7), pathRes);
-    if res
-        good = good + 1;
+%% Record or load all the test data
+if strcmp(user_mode,'record')%% record & test mode
+    % launch recognition system
+    [pathRes, probRes] = songreco(hmms, user_mode, file_name);
+    fprintf('Result :: %s\n',pathRes);
+    
+elseif strcmp(user_mode,'load') %%load  & test mode
+    good = 0;
+    for ifiles = 1:nfiles
+        file_name = list_file{2+ifiles};
+        % launch recognition system
+        [pathRes, probRes] = songreco(hmms, user_mode, file_name);
+        fprintf('Result :: %s\n',pathRes);
+        % is it the right result
+        if findstr(file_name(1:end-7), pathRes)
+            good = good + 1;
+        end
     end
+    fprintf('Correct :: %2d%%\n', round(good/nfiles*100));
 end
-
 
